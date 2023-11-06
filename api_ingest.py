5d827c8b661c22b254b6c14c8770cce78f0a8bcf
Maxwell <138258547+maxwellabgit@users.noreply.github.com>
2023-11-06 12:52:25 -0500

    Update api_ingest.py

--- api_ingest.py
+++ api_ingest.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import base64
 import pymssql
+import requests
 
 #retrieve credentials from Databricks Secrets. 
 secrets_scope = 'secrets_XXX_XXX'
@@ -25,7 +26,27 @@ conn = pymssql.connect(host = f'{server}:{port}\\{instance}',
     database=database)
 c = conn.cursor()
 
-#retrieve fieldnames
+#Alternate code for requests API call:
+url = 'https://123.gov/services/rest-api/gis-tool/123'
+login_url = 'https://123.gov/portal/rest/login'
+
+def pull_gis(url, login_url):
+    payload = {'f':'json',
+                'parameter1':'false',
+                'where':'clause',
+                'fields':'fields'}
+    r_login = requests.get(login_url,
+                        user = usr,
+                        password = base64.b64decode(pwd))
+    token = r_login.history[3].cookies.items()[0][1]
+    token_dict = {'token':token}
+    r = requests.get(url+'/query',
+                        params={**payload, **token_dict},
+                        user = usr,
+                        password = base64.b64decode(pwd))
+    return pd.json_normalize(r.json()['features']).reset_index(drop=True)
+
+#retrieve field names
 c.execute(q_fieldname)
 fields = c.fetchall()
 
@@ -36,6 +57,7 @@ data = c.fetchall()
 c.close()
 conn.close()
 
+#format field names to an acceptable format for spark
 fieldnames = [fields[i][3].replace(' ','_') for i in range(len(fields))]
 df_today = pd.DataFrame(result, columns = fieldnames)
 
@@ -46,5 +68,8 @@ for i in col_list:
 df_today['col2'] = np.where(df_today['col2'] == df_today['col1'], None, df_today['col2'])
 
 
-spark_df = spark.createDataFrame(df_today, verifySchema=False)
-spark_df.write.mode('append).saveAsTable("database.tablename")
+try:
+    spark_df = spark.createDataFrame(df_today, verifySchema=False)
+    spark_df.write.mode('append).saveAsTable("database.tablename")
+except Exception as e:
+    print(e)
